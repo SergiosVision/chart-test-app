31df8eb23c805eaeedf6fd9864ab2bdc3ead55d2
Sergey Syubar <ssyubar@kode-t.ru>
2023-04-24 17:23:35 +0700

    Fix spacing

--- src/modules/repositories/helpers/exceptions.ts
+++ src/modules/repositories/helpers/exceptions.ts
@@ -1,6 +1,7 @@
 import { ApplicationException } from '@utils/exception';
 
 export class GetRepositoriesListBaseError extends ApplicationException {}
+
 export class GetRepositoryDetailsBaseError extends ApplicationException {}
 
 export class GetWeeklyCommitCountBaseError extends ApplicationException {}
